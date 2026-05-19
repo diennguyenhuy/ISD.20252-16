3e1b8234f6315f4825be199239c34cf2d4930c6d
dimansego <dimansego213@gmail.com>
2026-05-19 23:15:15 +0700

    Pay order Test cases

--- Programming/backend/src/main/java/com/hust/soict/ict/aims/models/dto/response/product/BookDetail.java
+++ Programming/backend/src/main/java/com/hust/soict/ict/aims/models/dto/response/product/BookDetail.java
@@ -1,11 +1,12 @@
 package com.hust.soict.ict.aims.models.dto.response.product;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Getter
 @Setter
 @NoArgsConstructor
 public class BookDetail extends ProductDetail {
--- Programming/backend/src/main/java/com/hust/soict/ict/aims/models/dto/response/product/ProductDetail.java
+++ Programming/backend/src/main/java/com/hust/soict/ict/aims/models/dto/response/product/ProductDetail.java
@@ -1,11 +1,12 @@
 package com.hust.soict.ict.aims.models.dto.response.product;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-
+@Getter
 @Setter
 @NoArgsConstructor
 public abstract class ProductDetail {
