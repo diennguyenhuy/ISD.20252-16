d634132e7de9c958f7a3e6144b415a428f4db535
diennguyenhuy <diennguyenhuy6005@gmail.com>
2026-05-28 15:12:08 +0700

    fix mappers to support inheritance

--- Programming/backend/src/main/java/com/hust/soict/ict/aims/mapper/ProductMapper.java
+++ Programming/backend/src/main/java/com/hust/soict/ict/aims/mapper/ProductMapper.java
@@ -8,14 +8,16 @@ import java.util.List;
 
 @Mapper(
         componentModel = MappingConstants.ComponentModel.SPRING,
-        unmappedTargetPolicy = ReportingPolicy.ERROR
+        unmappedTargetPolicy = ReportingPolicy.ERROR,
+        subclassExhaustiveStrategy = SubclassExhaustiveStrategy.RUNTIME_EXCEPTION
 )
 public interface ProductMapper {
     @Mapping(target = "productType", ignore = true)
+    @Mapping(target = "id", expression = "java(product.getId().toString())")
     @SubclassMapping(source = Book.class, target = BookDetail.class)
     @SubclassMapping(source = Newspaper.class, target = NewspaperDetail.class)
     @SubclassMapping(source = CD.class, target = CDDetail.class)
-    @SubclassMapping(source = DVD.class, target = DVD.class)
+    @SubclassMapping(source = DVD.class, target = DVDDetail.class)
     ProductDetail toProductDetail(Product product);
 
     BookDetail toBookDetail(Book book);
